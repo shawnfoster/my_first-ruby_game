0b2b6d24e48301f9851765cbb9d587922076c8de
Shawn Foster <scenefoster@gmail.com>
2016-03-15 12:16:03 -0500

    ruby 101

---
+++ escape.rb
@@ -0,0 +1,2 @@
+single_quotes = 'Bill said, \'Wake up\''
+p single_quotes
---
+++ mad_libs.rb
@@ -0,0 +1,12 @@
+character = 'Super Shawn'
+place = 'Costa Rica'
+adventure = 'Surf Safari'
+goal = 'get a tube'
+enemy = 'reef'
+victory_speech = 'i just fucking got barreled'
+
+# use string interpolate (aka templating) to create a paragraph using variables
+
+paragraph = "#{character} flew into #{place} for a #{adventure} with the goal being to #{goal}. Watch out for the #{enemy} and steer clear but when he pulls it off he says #{victory_speech}"
+
+p paragraph
